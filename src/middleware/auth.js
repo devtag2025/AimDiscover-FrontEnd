41acb8ecdd6a1b7c078a05bcca89c1adfcbbc2dc
HassanAbbas10 <hassanabbas05674@gmail.com>
2025-11-27 11:55:27 +0500

    modified:   src/middleware/auth.js

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 
 export function authMiddleware(req) {
-  const token = req.cookies.get("token")?.value; // read from cookies
+  const token = req.cookies.get("accessToken")?.value; // read from cookies
 
   const protectedRoutes = ["/dashboard", "/profile", "/settings"];
   const isProtected = protectedRoutes.some((path) =>
